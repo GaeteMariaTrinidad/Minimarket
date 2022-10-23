64c256d7ff125ee4b94f189a36f8df13d7f446ba
bettolandro <a.diaze@duocuc.cl>
2022-10-23 14:32:40 -0300

    cambios carrito
    mostrar y eliminar, agregué variable total y se implementó interface

--- Minimarket/src/minimarket/Carrito.java
+++ Minimarket/src/minimarket/Carrito.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package minimarket;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,18 +12,23 @@ import java.util.Scanner;
  *
  * @author Trinidad Gaete
  */
-public class Carrito {
+public class Carrito implements CalculoCompra{
 
     private ArrayList<Producto> listaProductos;
+    int total;
 
     public Carrito() {
         listaProductos = new ArrayList<>();
     }
-    
-    public Carrito(ArrayList<Producto> listaProductos) {
-        this.listaProductos = listaProductos;
+
+    public int getTotal() {
+        return total;
     }
-    
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     /**
      * @return the listaProductos
      */
@@ -36,22 +42,22 @@ public class Carrito {
     public void setListaProductos(ArrayList<Producto> listaProductos) {
         this.listaProductos = listaProductos;
     }
-   
+
     public boolean validarProducto(String nombre) {
-        for (Producto p: listaProductos) {
+        for (Producto p : listaProductos) {
             if (p.nombre.equals(nombre)) {
-                return true;        
-            }  
+                return true;
+            }
         }
         return false;
-    } 
-    
-    public void agregarProducto(Scanner sc){
+    }
+
+    public void agregarProducto(Scanner sc) {
         System.out.println("Ingrese el nombre del produxto: ");
         String n = sc.next();
         boolean existe = this.validarProducto(n);
-        if(existe) {
-            for (Producto p: listaProductos) {
+        if (existe) {
+            for (Producto p : listaProductos) {
                 if (p.nombre.equals(n)) {
                     p.cantidad += 1;
                 }
@@ -63,22 +69,22 @@ public class Carrito {
             int pre = sc.nextInt();
             System.out.println("Ingrese la cantidad del procuto: ");
             int cant = sc.nextInt();
-            int sub = 0;   
+            int sub = 0;
             System.out.println("Tipo producto: 1- Carne \n 2- Verdura \n 3- Bebestible");
             int tipo = sc.nextInt();
-            switch (tipo){
+            switch (tipo) {
                 case 1:
                     System.out.println("Ingrese tipo de corte: ");
                     String tcorte = sc.next();
                     listaProductos.add(new Carne(tcorte, n, cod, pre, cant, sub));
                     break;
-                
+
                 case 2:
                     System.out.println("Ingrese valor nutricional: ");
                     String vn = sc.next();
                     listaProductos.add(new Verdura(vn, n, cod, pre, cant, sub));
                     break;
-                    
+
                 case 3:
                     System.out.println("Ingrese cantidad de ml: ");
                     int ml = sc.nextInt();
@@ -88,9 +94,64 @@ public class Carrito {
         }
         //aquí calcular el subtotal.
     }
-    
-    
-    
-    
-    
+
+    public void mostrarProducto() {
+        for (Producto p : listaProductos) {
+            if (p instanceof Carne) {
+                Carne ca = (Carne) p;
+                System.out.println("nombre carne: " + ca.nombre);
+                System.out.println("codigo: " + ca.codigo);
+                System.out.println("precio: " + ca.precio);
+                System.out.println("tipo corte: " + ca.getTipoCorte());
+                System.out.println("subtotal: " + ca.subtotal);
+
+            }
+            if (p instanceof Verdura) {
+                Verdura ve = (Verdura) p;
+                System.out.println("nombre verdura: " + ve.nombre);
+                System.out.println("codigo: " + ve.codigo);
+                System.out.println("precio: " + ve.precio);
+                System.out.println("valor nutricional: " + ve.getValorNutricional());
+                System.out.println("subtotal: " + ve.subtotal);
+
+            }
+            if (p instanceof Bebestible) {
+                Bebestible be = (Bebestible) p;
+                System.out.println("nombre bebestible: " + be.nombre);
+                System.out.println("codigo: " + be.codigo);
+                System.out.println("precio: " + be.precio);
+                System.out.println("cantidad ml: " + be.getCantidadMLitros());
+                System.out.println("subtotal: " + be.subtotal);
+
+            }
+        }
+
+    }
+
+    public void eliminarProducto(String nombre) {
+        int h = 0;
+        for (Producto p : listaProductos) {
+            boolean existe = this.validarProducto(nombre);
+            if (existe) {
+                h = 1;
+                listaProductos.remove(p);
+            }
+        }
+
+        if (h == 0) {
+            System.out.println("El producto no existe");
+        } else {
+            System.out.println("el producto ha sido eliminado");
+        }
+    }
+
+    @Override
+    public void precioFinalProducto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void totalProductos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
